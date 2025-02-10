984053d7ee419aa84bca8fcd61865f6bc3a36ddf
Ferraria-Alex <alexmferrari.a@gmail.com>
2025-02-10 15:05:49 +0100

    Exo02

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ParentComponentComponent } from './parent-component/parent-component.component';
+import { ListFriendsComponent } from './list-friends/list-friends.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'ex01', component: ParentComponentComponent },
+    { path: 'ex02', component: ListFriendsComponent },
 ];
---
+++ src/app/list-friends/one-friend/one-friend.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-one-friend',
+  imports: [],
+  templateUrl: './one-friend.component.html',
+  styleUrl: './one-friend.component.css'
+})
+export class OneFriendComponent {
+  firstName: string = "JEFF"; 
+  lastName: string = "The land shark";
+  status: string = "Active";
+  age: number = 22;
+  bio: string = "JEFF The land shark";
+  interests: string[] = ['Fish', 'Being cute', 'Water fights'];
+  reputation: string = "Cute But Will Bite";
+}
