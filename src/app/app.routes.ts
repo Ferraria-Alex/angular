990ca18ae94cb59da9dad4e3f4ca560ac9960747
Ferraria-Alex <alexmferrari.a@gmail.com>
2025-02-11 11:44:32 +0100

    Tp2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ParentComponentComponent } from './parent-component/parent-component.component';
+import { Tp2Component } from './tp2/tp2.component';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent },
+    { path: '', component: HomeComponent },
     { path: 'ex01', component: ParentComponentComponent },
+    { path: 'ex03', component: Tp2Component },
 ];
