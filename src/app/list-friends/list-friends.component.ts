ebb55556c14bb9e2bb009445f00b5a0c3dbbf324
Ferraria-Alex <alexmferrari.a@gmail.com>
2025-02-11 16:38:08 +0100

    Exo06

--- src/app/list-friends/list-friends.component.ts
+++ src/app/list-friends/list-friends.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { OneFriendComponent } from './one-friend/one-friend.component';
-import { NgIf } from '@angular/common';
+import { NgIf, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-list-friends',
-  imports: [OneFriendComponent,FormsModule,NgIf],
+  imports: [OneFriendComponent,FormsModule,NgIf,NgFor],
   templateUrl: './list-friends.component.html',
   styleUrl: './list-friends.component.css'
 })
 export class ListFriendsComponent {
+
+  listFriendsTab: { name: string; age: number; email: string }[] = [
+    { name: 'Alice', age: 30, email: 'alice@example.com' },
+    { name: 'Bob', age: 25, email: 'bob@example.com' },
+    { name: 'Charlie', age: 35, email: 'charlie@example.com' }
+  ];
+
   input: string = "";
   isAdded: boolean = false;
 
